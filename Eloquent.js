902d7232d1d8bb8d7d9136ac9c95d2d3345825f5
bellshafiq <bellshafiq@gmail.com>
2020-10-08 09:37:47 -0400

    Add files via upload
    file linked to index.html file

---
+++ Eloquent.js
@@ -0,0 +1,55 @@
+/*console.log("five"*2);
+console.log(false ? 1 :2)
+let aisha = "Happy ";
+let termon = "Happy Girl";
+console.log(aisha + termon);
+console.log(Math.min(3,56));*/
+/*let theNumber = 677;
+if (theNumber < 10)
+{
+    console.log("Small");
+   }
+else if (theNumber < 100)
+{
+    console.log("Medium");
+    }
+    else 
+    {
+        console.log("SmallBiLarge");
+        }
+let number = 0;
+while (number < 12)
+{
+    console.log (number);
+    number = number + 2;
+
+}
+let result = 1;
+let counter = 0;
+while (counter <10)
+{
+    counter += 1;
+    result *= 2;
+}
+console.log (result);
+
+for (let number= "#";number.length  <= 7;number += "#")
+{
+console.log(number);
+}*/
+for (i=1; i <=10; i+= 1)
+{
+var number = "";
+
+if (i % 2 == 0)
+{
+    number += "Shume";
+}
+if (i % 5 == 0)
+{
+    number += "Shafiq";
+   
+}
+console.log(number || i);
+}
+console.log("helloo");
